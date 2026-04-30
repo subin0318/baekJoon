617a51bdb4acfd8fb441ba9a6d71235ffb2bd65b
subin <156440512+subin0318@users.noreply.github.com>
2026-04-30 12:56:14 +0900

    Title: 비교 연산, Time: 0ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 260430/비교 연산/comparison-operator.py
@@ -0,0 +1,10 @@
+a,b=map(int,input().split())
+
+# 참인경우값 if 조건 else 거짓인경우값
+
+print(1) if a >= b else print(0)
+print(1) if a > b else print(0)
+print(1) if b >= a else print(0)
+print(1) if b > a else print(0)
+print(1) if a == b else print(0)
+print(1) if a != b else print(0)
