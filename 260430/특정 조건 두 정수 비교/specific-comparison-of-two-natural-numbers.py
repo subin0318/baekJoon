97af4ea22e8c46aec5a3198b28c8c6c7664777a4
subin <156440512+subin0318@users.noreply.github.com>
2026-04-30 14:08:39 +0900

    Title: 특정 조건 두 정수 비교, Time: 0ms, Memory: 0MB, Status: Passed - Codetree

--- 260430/특정 조건 두 정수 비교/specific-comparison-of-two-natural-numbers.py
+++ 260430/특정 조건 두 정수 비교/specific-comparison-of-two-natural-numbers.py
@@ -1,5 +1,8 @@
-a, b = map(int,input().split())
+a, b = map(int, input().split())
 
-if a < b : print(1,0 , end=" ")
-elif a == b : print(0,1 , end= " ")
-else : print(0 , 1 , end=" ")
+if a < b:
+    print(1, 0, end=" ")
+elif a == b:
+    print(0, 1, end=" ")
+else: # a > b 인 경우
+    print(0, 0, end=" ") # 예시: 앞선 두 케이스와 구분되는 값 출력
